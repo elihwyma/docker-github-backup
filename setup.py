daa88fc0d029574d72130711386f3e1f0d0d89fc
Amy While <26681721+elihwyma@users.noreply.github.com>
2023-05-17 12:45:58 +0100

    chore: update author

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='github-backup',
     version='1.0.0',
     description='',
-    url='https://github.com/lnxd/docker-github-backup',
-    author='lnxd',
+    url='https://github.com/elihwyma/docker-github-backup',
+    author='elihwyma',
     install_requires=['requests'],
     scripts=['github-backup.py'],
     zip_safe=True
